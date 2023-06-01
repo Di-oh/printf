5bb1cbdccbec4e855e5e8ad993a8e206728ef1eb
Dionisia Martin Silva <dionmart@cbr9s4.42barcelona.com>
2023-06-01 13:39:29 +0200

    aproximacion printf

--- ft_printf.c
+++ ft_printf.c
@@ -1,17 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dionmart <dionmart@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/01 10:48:33 by dionmart          #+#    #+#             */
-/*   Updated: 2023/06/01 11:01:34 by dionmart         ###   ########.fr       */
+/*   Created: 2023/06/01 12:34:12 by dionmart          #+#    #+#             */
+/*   Updated: 2023/06/01 13:32:03 by dionmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "printf.h"
 
-int	ft_printf(const char *format, ...)
+int ft_printf(const char *format, ...)
 {
-	va_list my_list;
-	va_start(my_list,)
+	int 		num_arg;
+	va_list	my_lis;
+
+	num_arg = ft_count(format);
+	va_start(my_list, num_arg);
+	va_arg();
+
+
+	return (i);
 }
